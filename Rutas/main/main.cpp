030ccd5ac3640e20050d0fff8c385de09e571492
Andrés Mora <jose.mora.mora@est.una.ac.cr>
2025-11-05 23:13:00 -0600

    Implement css

--- Rutas/main/main.cpp
+++ Rutas/main/main.cpp
@@ -7,6 +7,11 @@ int main(int argc, char* argv[])
 {
 	QApplication app(argc, argv);
 
+	QFile file("ben10style.qss");
+	if (file.open(QFile::ReadOnly)) {
+		app.setStyleSheet(file.readAll());
+	}
+
 	LogInView window;
 	window.show();
 
